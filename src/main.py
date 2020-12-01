8381ba5fa1cc7f9d791e8e55665c3b4191d14ed1
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-12-01 12:44:56 +0200

    UI done

--- src/main.py
+++ src/main.py
@@ -10,26 +10,87 @@ PROJECT_ID = int(os.environ["modal.state.slyProjectId"])
 
 PROJECT = None
 RES_PROJECT = None
-RES_PROJECT_NAME = None
 META: sly.ProjectMeta = None
+UI_TAGS = None
+UI_CLASSES = None
 
-
-@my_app.callback("generate")
-@sly.timeit
-def generate_random_string(api: sly.Api, task_id, context, state, app_logger):
-    rand_string = ''.join((random.choice(string.ascii_letters + string.digits)) for _ in range(LENGTH))
-    rand_string = state["prefix"] + rand_string
-    api.task.set_field(task_id, "data.randomString", rand_string)
-
-
-@my_app.callback("preprocessing")
+@my_app.callback("copy_tags")
 @sly.timeit
 def preprocessing(api: sly.Api, task_id, context, state, app_logger):
-    sly.logger.info("do something here")
+    global RES_PROJECT
+    RES_PROJECT = api.project.create(WORKSPACE_ID, state["resultProjectName"], change_name_if_conflict=True)
+    my_app.logger.info("Result Project is created (name={!r}; id={})".format(RES_PROJECT.name, RES_PROJECT.id))
+
+    api.project.update_meta(RES_PROJECT.id, RES_META.to_json())
+
+    allowed_tags = set()
+    for tag, allowed in zip(UI_TAGS, state["tagsSelected"]):
+        if allowed:
+            allowed_tags.add(tag["name"])
+
+    allowed_classes = set()
+    for obj_class, allowed in zip(UI_CLASSES, state["classesSelected"]):
+        if allowed:
+            allowed_classes.add(obj_class["title"])
+
+    progress = sly.Progress("Processing", PROJECT.images_count, ext_logger=my_app.logger)
+    for dataset in api.dataset.get_list(PROJECT.id):
+        res_dataset = api.dataset.create(RES_PROJECT.id, dataset.name)
+        ds_images = api.image.get_list(dataset.id)
+        for batch in sly.batched(ds_images):
+            image_ids = [image_info.id for image_info in batch]
+            image_names = [image_info.name for image_info in batch]
+            image_metas = [image_info.meta for image_info in batch]
+
+            ann_infos = api.annotation.download_batch(dataset.id, image_ids)
+            anns = [sly.Annotation.from_json(ann_info.annotation, META) for ann_info in ann_infos]
+
+            original_ids = []
+            res_image_names = []
+            res_anns = []
+            res_metas = []
+
+            for image_id, image_name, image_meta, ann in zip(image_ids, image_names, image_metas, anns):
+                tag: sly.Tag = ann.img_tags.get(IMAGE_TAG_NAME)
+                if tag is None:
+                    my_app.logger.warn("Image {!r} in dataset {!r} doesn't have tag {!r}. Image is skipped"
+                                       .format(image_name, dataset.name, IMAGE_TAG_NAME))
+                    progress.iter_done_report()
+                    continue
+
+                csv_row = CSV_INDEX.get(str(tag.value), None)
+                if csv_row is None:
+                    my_app.logger.warn(
+                        "Match not found (id={}, name={!r}, dataset={!r}, tag_value={!r}). Image is skipped"
+                        .format(image_id, image_name, dataset.name, str(tag.value)))
+                    progress.iter_done_report()
+                    continue
+
+                res_ann = ann.clone()
+                res_meta = image_meta.copy()
+
+                if ASSIGN_AS == "tags":
+                    res_ann = assign_csv_row_as_tags(image_id, image_name, res_ann, csv_row)
+                else:  # metadata
+                    res_meta = assign_csv_row_as_metadata(image_id, image_name, image_meta, csv_row)
+
+                original_ids.append(image_id)
+                res_image_names.append(image_name)
+                res_anns.append(res_ann)
+                res_metas.append(res_meta)
+
+            res_image_infos = api.image.upload_ids(res_dataset.id, res_image_names, original_ids, metas=res_metas)
+            res_image_ids = [image_info.id for image_info in res_image_infos]
+            api.annotation.upload_anns(res_image_ids, res_anns)
+            progress.iters_done_report(len(res_image_ids))
+
+    api.task.set_output_project(my_app.task_id, RES_PROJECT.id, RES_PROJECT.name)
 
 
 def prepare_ui_tags():
-    tags = []
+    global UI_TAGS
+
+    UI_TAGS = []
     tags_selected = []
     tags_disabled = []
     disabled_message = []
@@ -37,7 +98,7 @@ def prepare_ui_tags():
     for tag_meta in META.tag_metas:
         tag_meta: sly.TagMeta
         cur_json = tag_meta.to_json()
-        tags.append(cur_json)
+        UI_TAGS.append(cur_json)
         if tag_meta.applicable_to == TagApplicableTo.ALL:
             tags_selected.append(True)
             tags_disabled.append(False)
@@ -50,38 +111,39 @@ def prepare_ui_tags():
             tags_selected.append(False)
             tags_disabled.append(True)
             disabled_message.append("applicable only to objects")
-    return tags, tags_selected, tags_disabled, disabled_message
+    return UI_TAGS, tags_selected, tags_disabled, disabled_message
 
 
 def prepare_ui_classes():
-    classes = []
+    global UI_CLASSES
+    UI_CLASSES = []
     classes_selected = []
     for obj_class in META.obj_classes:
         obj_class: sly.ObjClass
-        classes.append(obj_class.to_json())
+        UI_CLASSES.append(obj_class.to_json())
         classes_selected.append(True)
-    return classes, classes_selected
+    return UI_CLASSES, classes_selected
 
 
 def main():
-    global PROJECT, META, RES_PROJECT_NAME
+    global PROJECT, META
 
     api = sly.Api.from_env()
     PROJECT = api.project.get_info_by_id(PROJECT_ID)
     META = sly.ProjectMeta.from_json(api.project.get_meta(PROJECT_ID))
 
-    RES_PROJECT_NAME = PROJECT.name + " (objects with tags)"
+    res_project_name = PROJECT.name + " (objects with tags)"
 
-    tags, tags_selected, tags_disabled, disabled_message = prepare_ui_tags()
-    classes, classes_selected = prepare_ui_classes()
+    _, tags_selected, tags_disabled, disabled_message = prepare_ui_tags()
+    _, classes_selected = prepare_ui_classes()
 
     data = {
         "projectId": PROJECT.id,
         "projectName": PROJECT.name,
         "projectPreviewUrl": api.image.preview_url(PROJECT.reference_image_url, 100, 100),
-        "tags": tags,
+        "tags": UI_TAGS,
         "tagsDisabledMessage": disabled_message,
-        "classes": classes,
+        "classes": UI_CLASSES,
         "resultProjectId": "",
         "resultProjectPreviewUrl": "",
         "started": False,
@@ -93,11 +155,12 @@ def main():
         "tagsSelected": tags_selected,
         "tagsDisabled": tags_disabled,
         "classesSelected": classes_selected,
-        "resultProjectName": RES_PROJECT_NAME,
+        "resultProjectName": res_project_name,
+        "resolve": "skip"
     }
 
     # Run application service
-    my_app.run(data=data, state=state, initial_events=[{"command": "preprocessing"}])
+    my_app.run(data=data, state=state)
 
 
 if __name__ == "__main__":
